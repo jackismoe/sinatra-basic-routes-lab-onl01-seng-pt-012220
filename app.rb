b42b7d2454732ecde7c5a90da62bf76fac1ff251
ohjacksn <jacksonmo@me.com>
2020-04-24 09:47:23 -0400

    lab update

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Jackson."
+    end
+
+    get '/hometown' do
+        "My hometown is Atlanta, GA."
+    end
+
+    get('/favorite-song') {"My favorite song is Astrovan by Mt. Joy."}
 end
